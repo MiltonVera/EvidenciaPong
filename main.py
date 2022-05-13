15b5c2fe640eb9038248edcce476e06aa89af4fb
MiltonVera <a00833471@tec.mx>
2022-05-13 11:41:59 -0500

    flappybird

--- main.py
+++ main.py
@@ -1,60 +1,65 @@
 from turtle import *
+from time import *
+from random import randrange
+
+
+pantalla = Screen()
+pantalla.title("Flappy Bird")
+pantalla.bgcolor("black")
+pantalla.setup(width=800, height = 600)
+# pantalla.tracer(0)
+
+#Tubos cruzados
+puntaje = 0
+
+#Dibujamos el pajaro
+pajaro = Turtle()
+pajaro.speed(0)
+pajaro.shape("circle")
+pajaro.color("white")
+pajaro.penup()
+pajaro.goto(0,0)
+quieto = True
+
+def caida():
+    y = pajaro.ycor()
+    y-=5
+    pajaro.sety(y)
+def subir():
+    y = pajaro.ycor()
+    y+=10
+    pajaro.sety(y)
+
+def dibujarObstaculo():
+    rectanguloBlanco = Turtle()
+    rectanguloBlanco.speed(1)
+    rectanguloBlanco.shape("square")
+    rectanguloBlanco.color("white")
+    rectanguloBlanco.shapesize(stretch_wid=30, stretch_len=3)
+    rectanguloBlanco.penup()
+
+    margen = Turtle()
+    margen.speed(1)
+    margen.shape("square")
+    margen.color("black")
+    margen.shapesize(stretch_wid=5, stretch_len=3)
+    margen.penup()
+    
+
+    
+    
 
 
 
-# Variables para almacenar los puntos de los jugadores
-puntos_l = 0
-puntos_r = 0
-
-#Creamos la pantalla
-pantalla = Screen()#creamos la instancia de la pantalla
-pantalla.title("Pong")#Definimos el titulo de la ventana
-pantalla.bgcolor("black")#Colocamos el fondo como negro
-pantalla.setup(width=1000, height=600)#Definimos las dimensiones de la patalla
-speed(0)
-
-cuadradol = Turtle()#Creamos el objeto que dibujara 
-cuadradol.shape("square")#Definimos que dibuje un cuadrado
-cuadradol.color("white")#Indicamos el color del cuadrado como blanco
-cuadradol.shapesize(stretch_wid=8, stretch_len=2)#Definimos las dimensiones del cuadrado
-cuadradol.penup()#Levantamos el lapiz
-cuadradol.goto(-400,0)
-
-cuadrador = Turtle()#Se crea un segundo cuadrado
-cuadrador.shape("square")
-cuadrador.color("white")
-cuadrador.shapesize(stretch_wid=8, stretch_len=2)
-cuadrador.penup()
-cuadrador.goto(400,0)
-
-
-def moveUpL():
-    y = cuadradol.ycor()
-    y += 20;
-    cuadradol.sety(y)
-
-def moveDownL():
-    y = cuadradol.ycor()
-    y -= 20;
-    cuadradol.sety(y)
-
-def moveUpR():
-    y = cuadrador.ycor()
-    y += 20;
-    cuadrador.sety(y)
-
-def moveDownR():
-    y = cuadrador.ycor()
-    y -= 20;
-    cuadrador.sety(y)
-
+dibujarObstaculo()
 
 pantalla.listen()
-pantalla.onkeypress(moveUpL, "w")
-pantalla.onkeypress(moveDownL, "s")
+pantalla.onkeypress(subir,"w")
 
-pantalla.onkeypress(moveUpR, "Up")
-pantalla.onkeypress(moveDownR, "Down")
+while(pajaro.ycor() > -600):
+    pantalla.update()
+    sleep(0.01)
+    caida()
 
-done()
+    
 
