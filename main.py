5ebc58669d935cfdbb2136237b3e08823a6ad32c
MiltonVera <a00833471@tec.mx>
2022-05-13 10:05:07 -0500

    animacion

--- main.py
+++ main.py
@@ -6,22 +6,21 @@ from turtle import *
 puntos_l = 0
 puntos_r = 0
 
-pantalla = Screen()
-pantalla.title("Pong")
-pantalla.bgcolor("black")
-pantalla.setup(width=1000, height=600)
+#Creamos la pantalla
+pantalla = Screen()#creamos la instancia de la pantalla
+pantalla.title("Pong")#Definimos el titulo de la ventana
+pantalla.bgcolor("black")#Colocamos el fondo como negro
+pantalla.setup(width=1000, height=600)#Definimos las dimensiones de la patalla
+speed(0)
 
-
-cuadradol = Turtle()
-cuadradol.speed(0)
-cuadradol.shape("square")
-cuadradol.color("white")
-cuadradol.shapesize(stretch_wid=8, stretch_len=2)
-cuadradol.penup()
+cuadradol = Turtle()#Creamos el objeto que dibujara 
+cuadradol.shape("square")#Definimos que dibuje un cuadrado
+cuadradol.color("white")#Indicamos el color del cuadrado como blanco
+cuadradol.shapesize(stretch_wid=8, stretch_len=2)#Definimos las dimensiones del cuadrado
+cuadradol.penup()#Levantamos el lapiz
 cuadradol.goto(-400,0)
 
-cuadrador = Turtle()
-cuadrador.speed(0)
+cuadrador = Turtle()#Se crea un segundo cuadrado
 cuadrador.shape("square")
 cuadrador.color("white")
 cuadrador.shapesize(stretch_wid=8, stretch_len=2)
