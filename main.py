33dfe8d8d14d611edfdfc28620ef1670036ba5e3
MiltonVera <a00833471@tec.mx>
2022-05-13 12:55:34 -0500

    correr

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+from operator import truediv
 from turtle import *
 from time import *
 from random import randrange
+
+import asyncio
 
 
 pantalla = Screen()
 pantalla.title("Flappy Bird")
 pantalla.bgcolor("black")
 pantalla.setup(width=800, height = 600)
+
+yins = [0, 0, 0]
+yfins = [0, 0, 0]
+rectangulos1 = [0, 0, 0]
+rectangulos2 = [0, 0, 0]
+
+
+
+#margenes = []
 # pantalla.tracer(0)
 
 #Tubos cruzados
@@ -31,35 +43,105 @@ def subir():
     pajaro.sety(y)
 
 def dibujarObstaculo():
-    rectanguloBlanco = Turtle()
-    rectanguloBlanco.speed(1)
-    rectanguloBlanco.shape("square")
-    rectanguloBlanco.color("white")
-    rectanguloBlanco.shapesize(stretch_wid=30, stretch_len=3)
-    rectanguloBlanco.penup()
+    x = 300
+    altura = randrange(300,600,10)
+    margen = 200
 
-    margen = Turtle()
-    margen.speed(1)
-    margen.shape("square")
-    margen.color("black")
-    margen.shapesize(stretch_wid=5, stretch_len=3)
-    margen.penup()
+    yin = altura-margen
+    yfin = altura
+
+    rectangulo1 = Turtle()
+    rectangulo1.speed(0)
+    rectangulo1.penup()
+    rectangulo1.goto(x,altura)
+    rectangulo1.shape("square")
+    rectangulo1.color("white")
+    rectangulo1.shapesize(stretch_wid=30, stretch_len=3)
     
 
-    
-    
+    rectangulo2 = Turtle()
+    rectangulo2.speed(0)
+    rectangulo2.penup()
+    rectangulo2.goto(x,altura-margen-600)
+    print(altura-margen-600)
+    rectangulo2.shape("square")
+    rectangulo2.color("white")
+    rectangulo2.shapesize(stretch_wid=30, stretch_len=3)
+
+    #Margen de 200 pixeles
+    return rectangulo1,rectangulo2,yin,yfin
 
 
-
-dibujarObstaculo()
 
 pantalla.listen()
 pantalla.onkeypress(subir,"w")
 
+def moverObstaculo(rectangulos1,rectangulos2, i):
+    xrectangulo1 = rectangulos1[i].xcor()
+    xrectangulo2 = rectangulos2[i].xcor()
+
+    xrectangulo1 -=10
+    xrectangulo2 -= 10
+
+    rectangulos1[i].setx(xrectangulo1)
+    rectangulos2[i].setx(xrectangulo2)
+
+frame = 1
+index = 0
+
+activados = [False,False,False]
+
 while(pajaro.ycor() > -600):
     pantalla.update()
+    if(frame%80 == 0 or frame == 1):
+        
+        rectangulo1,rectangulo2,yin,yfin = dibujarObstaculo()
+
+        
+        # rectangulos1[index] = rectangulo1
+        # rectangulos2[index] = rectangulo2
+        # yins[index] = yin
+        # yfins[index] = yfins
+        # index += 1
+
+        moverObstaculo(rectangulos1,rectangulos2,index)
+
+        if (index >= 2):
+            index = 0
+        
+
+    #     rectangulos1.append(rectangulo1)
+    #     rectangulos2.append(rectangulo2)
+    #     yins.append(yin)
+    #     yfins.append(yfin)
+        
+    #     print(activados)
+    #     if(frame%30 == 0 or frame==1):
+    #         if(activados[0]):
+    #             if(activados[1]):
+    #                 if(not activados[2]):
+    #                     activados[2] = True
+    #             else:
+    #                 activados[1] = True
+    #         else:
+    #             activados[0] = True
+            
+
+    # if (activados[0]):
+    #     moverObstaculo(rectangulos1,rectangulos2,0)
+
+    # if (activados[1]):
+    #     moverObstaculo(rectangulos1,rectangulos2,1)
+
+    # if (activados[2]):
+    #     moverObstaculo(rectangulos1,rectangulos2,2)
+
+    
+
     sleep(0.01)
     caida()
+    frame+=1
+
 
     
 
